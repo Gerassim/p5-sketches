8dbffc1e9f66a6799ee2e10055c6b8055d6fbb10
Oleksii Herashchenko <a.heraschenko@meteoprog.ua>
2017-04-27 15:38:27 +0300

    Refactor of game

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,244 +1,223 @@
 /**
  * Created by gerassum on 26.04.17.
  */
-var field = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-];
 
-// var field = [
-//     [1, 2, 1, 2],
-//     [2, 1, 2, 1],
-//     [1, 2, 1, 2],
-//     [2, 1, 2, 1],
-// ];
-
-var emptyField = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-];
-
-var directions = ['up', 'right', 'down', 'left'];
-
-var colors = [
-    '#000000',
-    '#0000FF',
-    '#FF00FF',
-    '#FFFF00',
-    '#008000',
-    '#00FF00',
-    '#800000',
-    '#000080',
-    '#808000',
-    '#800080',
-    '#FF0000',
-
-];
+field = new Field(800, 800);
 
 function keyPressed() {
     if (keyCode === BACKSPACE) {
         spawnNew();
     } else if (keyCode === UP_ARROW) {
-        move(directions[0]);
+        field.move(createVector(-1, 0));
     } else if (keyCode === DOWN_ARROW) {
-        move(directions[2]);
+        field.move(createVector(1, 0));
     } else if (keyCode === LEFT_ARROW) {
-        move(directions[3]);
+        field.move(createVector(0, -1));
     } else if (keyCode === RIGHT_ARROW) {
-        move(directions[1]);
+        field.move(createVector(0, 1));
     }
 }
 
 function setup() {
-    createCanvas(800, 800);
-    rows = field.length;
-    colls = field[0].length;
-    cellWidth = width / 4;
-    cellHeight = height / 4;
-    spawnNew(2);
-
-    movesLeft = isMovesLeft();
+    createCanvas(field.fieldWidht, field.fieldHeight);
+    field.init(false);
 }
 
 function draw() {
-
-    background(51);
-    for (var j = 0; j < rows; j++) {
-        for (var i = 0; i < colls; i++) {
-            fill(colors[field[i][j]]);
-            noStroke();
-            rectMode(CORNER);
-            rect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
-            fill(0);
-            textSize(50);
-            textAlign(CENTER, CENTER);
-            rectMode(RADIUS);
-
-            if (field[i][j] > 0) {
-                text(Math.pow(2, field[i][j]), (j) * cellWidth, (i) * cellHeight, cellWidth, cellHeight)
-            }
-        }
-    }
-
-    if(!movesLeft) {
-        fill(0);
-        textSize(150);
-        textAlign(CENTER, CENTER);
-        rectMode(RADIUS);
-        text("GAME OVER", 0, 0, width, height)
-    }
+    field.draw()
 }
 
-function spawnNew(amount) {
-    for (var i = 0; i < amount; i++) {
-        spawnCell(floor(random(2)) + 1);
-    }
-}
+function Field(fieldWidth, fieldHeight) {
+    this.field = [];
+    this.rows = 4;
+    this.cols = 4;
 
-function spawnCell(value) {
-    var row, coll, tries = 0, place = false, placesCount = 0;
+    this.fieldWidht = fieldWidth;
+    this.fieldHeight = fieldHeight;
+    this.cellWidth = fieldWidth / this.cols;
+    this.cellHeight = fieldHeight / this.rows;
 
-    for (var j = 0; j < rows; j++) {
-        for (var i = 0; i < colls; i++) {
-            if (field[i][j] === 0) {
-                place = true;
-                placesCount++;
+    this.init = function (empty) {
+
+        for (var i = 0; i < this.rows; i++) {
+            this.field[i] = [];
+            for (var j = 0; j < this.cols; j++) {
+                this.field[i][j] = new Cell(0);
             }
         }
-    }
 
-    if (place) {
+        if (!empty) {
+            this.spawnNew(floor(random(2)) + 1);
+        }
+    };
+
+    this.spawnNew = function (amount) {
+        for (var i = 0; i < amount; i++) {
+            var emptyCell = this.getRandomEmptyCell();
+            if (emptyCell)
+                emptyCell.setVal(floor(random(2)) + 1);
+        }
+    };
+
+    this.draw = function () {
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                fill(this.field[i][j].getColor());
+                noStroke();
+                rectMode(CORNER);
+                rect(j * this.cellHeight, i * this.cellWidth,
+                    this.cellWidth, this.cellHeight);
+                fill(0);
+                textSize(50);
+                textAlign(CENTER, CENTER);
+                rectMode(RADIUS);
+                if (this.field[i][j].val > 0) {
+                    text(this.field[i][j].getText(), j * this.cellHeight, i * this.cellWidth,
+                        this.cellWidth, this.cellHeight)
+                }
+            }
+        }
+    };
+
+    this.getRandomEmptyCell = function () {
+        var places = 0, row, col;
+
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                if (this.field[i][j].isEmpty()) {
+                    places++;
+                    break;
+                }
+            }
+            if (places > 0) break;
+        }
+
+        if (places > 0) {
+            do {
+                row = floor(random(this.rows));
+                col = floor(random(this.cols));
+            } while (!this.field[row][col].isEmpty());
+
+            return this.field[row][col];
+        } else {
+            return false;
+        }
+    };
+
+    this.move = function (direction) {
+        var noChanges, counter = -1;
+
         do {
-            row = floor(random(rows));
-            coll = floor(random(colls));
-            tries++;
-        } while (field[row][coll] > 0);
+            counter++;
+            noChanges = true;
 
-        field[row][coll] = value;
-    }
-}
-
-function move(moveName) {
-    var newField = emptyField.clone();
-    var moveDirection;
-
-    switch (moveName) {
-        case 'up':
-            moveDirection = createVector(-1, 0);
-            break;
-        case 'down':
-            moveDirection = createVector(1, 0);
-            break;
-        case 'left':
-            moveDirection = createVector(0, -1);
-            break;
-        case 'right':
-            moveDirection = createVector(0, 1);
-            break;
-    }
-
-    if (moveName === 'up' || moveName === "left") {
-        for (var j = 0; j < rows; j++) {
-            for (var i = 0; i < rows; i++) {
-                moveCell(j, i, moveDirection, newField);
-            }
-        }
-    } else {
-        for (j = rows - 1; j >= 0; j--) {
-            for (i = rows - 1; i >= 0; i--) {
-                moveCell(j, i, moveDirection, newField);
-            }
-        }
-    }
-
-    for (j = 0; j < rows; j++) {
-        for (i = 0; i < rows; i++) {
-            if(field[j][i] !== newField[j][i]) {
-                // something actually moved
-
-                field = newField;
-                spawnNew(floor(random(2)) + 1);
-            }
-        }
-    }
-
-    movesLeft = isMovesLeft();
-}
-
-function moveCell(j, i, moveDirection, newField) {
-    var moving = true;
-    var point = createVector(j, i);
-    var value = field[point.x][point.y];
-
-    if (value > 0) {
-        while (moving) {
-            var newPos = point.add(moveDirection);
-            if (field[newPos.x] === undefined || field[newPos.y] === undefined || (newField[newPos.x][newPos.y] !== value && newField[newPos.x][newPos.y] !== 0)) {
-                newPos.sub(moveDirection);
-                moving = false;
-            } else if (newField[newPos.x][newPos.y] === value) {
-                value++;
-                moving = false;
-            }
-        }
-
-        newField[newPos.x][newPos.y] = value;
-    }
-}
-
-function isMovesLeft() {
-    var isMovesLeft = false;
-
-    for (var j = 0; j < rows; j++) {
-        for (var i = 0; i < rows; i++) {
-            if (field[j - 1] !== undefined) {
-                if (field[i] !== undefined) {
-                    if (field[j][i] === field[j - 1][i] || field[j - 1][i] === 0) {
-                        isMovesLeft = true;
+            if (direction.x > 0 || direction.y > 0) {
+                for (var i = this.rows - 1; i >= 0; i--) {
+                    for (var j = this.cols - 1; j >= 0; j--) {
+                        if (this.canMove(createVector(i, j), direction)) {
+                            this.performMove(createVector(i, j), direction);
+                            noChanges = false;
+                        }
+                    }
+                }
+            } else {
+                for (i = 0; i < this.rows; i++) {
+                    for (j = 0; j < this.cols; j++) {
+                        if (this.canMove(createVector(i, j), direction)) {
+                            this.performMove(createVector(i, j), direction);
+                            noChanges = false;
+                        }
                     }
                 }
             }
+        } while (!noChanges);
 
-            if (field[j + 1] !== undefined) {
-                if (field[i] !== undefined) {
-                    if (field[j][i] === field[j + 1][i] || field[j + 1][i] === 0) {
-                        isMovesLeft = true;
-                    }
+        this.resetMergeConditions();
+        if (counter > 0) {
+            this.spawnNew(floor(random(2)) + 1);
+        }
+    };
+
+    this.hasMovesLeft = function () {
+
+    };
+
+    this.resetMergeConditions = function () {
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                this.field[i][j].acceptMerge = true;
+            }
+        }
+    };
+
+    this.canMove = function (initial, direction) {
+        var initialX = initial.x;
+        var initialY = initial.y;
+
+        initial.add(direction);
+
+        if (this.field[initialX][initialY].val > 0) {
+            if (this.field[initial.x] !== undefined && this.field[initial.y] !== undefined) {
+                if ((this.field[initial.x][initial.y].val === this.field[initialX][initialY].val
+                    && this.field[initial.x][initial.y].acceptMerge && this.field[initialX][initialY].acceptMerge)
+                    || this.field[initial.x][initial.y].val === 0) {
+                    return true;
                 }
             }
+        }
 
-            if (field[j] !== undefined) {
-                if (field[i + 1] !== undefined) {
-                    if (field[j][i] === field[j][i + 1] || field[j][i + 1] === 0) {
-                        isMovesLeft = true;
-                    }
-                }
-            }
+        return false;
+    };
 
-            if (field[j] !== undefined) {
-                if (field[i - 1] !== undefined) {
-                    if (field[j][i] === field[j][i - 1] || field[j][i - 1] === 0) {
-                        isMovesLeft = true;
-                    }
-                }
-            }
+    this.performMove = function (initial, direction) {
+        var initialX = initial.x;
+        var initialY = initial.y;
 
+        initial.add(direction);
+
+        if (this.field[initial.x][initial.y].val === this.field[initialX][initialY].val) {
+            this.field[initialX][initialY].val = 0;
+            this.field[initial.x][initial.y].val++;
+            this.field[initial.x][initial.y].acceptMerge = false;
+        } else {
+            this.field[initial.x][initial.y].val = this.field[initialX][initialY].val;
+            this.field[initial.x][initial.y].acceptMerge = false;
+            this.field[initialX][initialY].val = 0;
         }
     }
-
-    return isMovesLeft;
 }
-Array.prototype.clone = function () {
-    var arr = this.slice(0);
-    for (var i = 0; i < this.length; i++) {
-        if (this[i].clone) {
-            //recursion
-            arr[i] = this[i].clone();
-        }
+
+function Cell(value) {
+    this.val = value;
+    this.acceptMerge = true;
+
+    this.colorPalette = [
+        '#000000',
+        '#0000FF',
+        '#FF00FF',
+        '#FFFF00',
+        '#008000',
+        '#00FF00',
+        '#800000',
+        '#000080',
+        '#808000',
+        '#800080',
+        '#FF0000',
+    ];
+
+    this.getColor = function () {
+        return this.colorPalette[this.val];
+    };
+
+    this.setVal = function (value) {
+        this.val = value;
+    };
+
+    this.getText = function () {
+        return Math.pow(2, this.val);
+    };
+
+    this.isEmpty = function () {
+        return !this.val > 0;
     }
-    return arr;
-};
+}
